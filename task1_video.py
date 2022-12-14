efcb12adfd09bbcc2b8a180c1fa068010396d299
anan <anangupta05@gmail.com>
2022-12-15 00:30:47 +0530

    -

--- task1_video.py
+++ task1_video.py
@@ -4,21 +4,22 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv.VideoCapture('crowd.mp4')
 
-while 1:
-	_, img = cap.read()
-	
+status, img = cap.read()
+while status:
 	#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-	#faces = face_cascade.detectMultiScale(img, 1.1, 1)
+	#faces = face_cascade.detectMultiScale(img, 1.1, 10)
 	
-	faces = face_cascade.detectMultiScale(img, 1.1, 1)
+	faces = face_cascade.detectMultiScale(img, 1.1, 10)
 	
 	for (x, y, w, h) in faces:
 		cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 		
 	cv.imshow('img', img)
 	
-	k = cv.waitKey(30)
+	k = cv.waitKey(3)
 	if k == 27:
 		break
-		
+	
+	status, img = cap.read()
+
 cap.release()
